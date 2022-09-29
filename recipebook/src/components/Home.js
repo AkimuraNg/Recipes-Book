3ebde3148ae82b4734c51b6008023caff9127d25
HNDuy Nguyen <duynguyen11062001@gmail.com>
2022-09-29 15:27:14 +0300

    Edit landing page

--- recipebook/src/components/Home.js
+++ recipebook/src/components/Home.js
@@ -15,16 +15,24 @@ const Home = () => {
                     <h1>Recipes Book</h1>
                     <p>Welcome to Recipes Book</p>
                 </section>
-                
-                {/*Cooking home view*/}
-                <section className="cooking">
-                    <h3>Cooking Recipes</h3>
-                </section>
+                <div className="container-fluid view">
+                    <div className="row justify-content-between" >
+                        <div className="col-2" style={{border: "1px solid black", marginLeft: 20}}>
+                            Recommend for You
+                        </div>
+                        <div className="col-9" >
+                            {/*Cooking home view*/}
+                            <section className="cooking" style={{border: "1px solid black"}}>
+                                <h3>Cooking Recipes</h3>
+                            </section>
 
-                {/*Cocktail home view*/}
-                <section className="cocktail">
-                    <h3>Cocktail Recipes</h3>
-                </section>
+                            {/*Cocktail home view*/}
+                            <section className="cocktail" style={{border: "1px solid black"}}>
+                                <h3>Cocktail Recipes</h3>
+                            </section>
+                        </div>
+                    </div>
+                </div>
             </main>
         </div>
     )
