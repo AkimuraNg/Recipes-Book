2e611bb31186110713c839c06b628409e6b52ac1
HNDuy Nguyen <duynguyen11062001@gmail.com>
2022-09-30 13:35:43 +0300

    Edit landing page

--- recipebook/src/components/Home.js
+++ recipebook/src/components/Home.js
@@ -16,19 +16,88 @@ const Home = () => {
                     <p>Welcome to Recipes Book</p>
                 </section>
                 <div className="container-fluid view">
-                    <div className="row justify-content-between" >
-                        <div className="col-2" style={{border: "1px solid black", marginLeft: 20}}>
-                            Recommend for You
+                    <div className="row justify-content-evenly" >
+                        <div className="col-2 recommend">
+                            <h3>Recommend for You</h3>
+                            <div className="card">
+                                    <div className="card-body">
+                                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    </div>
+                            </div>
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div>
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div>
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div>
                         </div>
-                        <div className="col-9" >
+                        <div className="col-7" >
                             {/*Cooking home view*/}
-                            <section className="cooking" style={{border: "1px solid black"}}>
+                            <section className="cooking" style={{ border: "1px solid black" }}>
                                 <h3>Cooking Recipes</h3>
+                                <div className="items">
+                                    <div className="card">
+                                        <div className="card-body">
+                                            <h5 className="card-title">Card title</h5>
+                                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                                        </div>
+                                    </div>
+                                    <div className="card">
+                                        <div className="card-body">
+                                            <h5 className="card-title">Card title</h5>
+                                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                                        </div>
+                                    </div>
+                                    <div className="card">
+                                        <div className="card-body">
+                                            <h5 className="card-title">Card title</h5>
+                                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                                        </div>
+                                    </div>
+                                </div>
                             </section>
 
                             {/*Cocktail home view*/}
-                            <section className="cocktail" style={{border: "1px solid black"}}>
+                            <section className="cocktail" style={{ border: "1px solid black" }}>
                                 <h3>Cocktail Recipes</h3>
+                                <div className="items">
+                                    <div className="card">
+                                        <div className="card-body">
+                                            <h5 className="card-title">Card title</h5>
+                                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                                        </div>
+                                    </div>
+                                    <div className="card">
+                                        <div className="card-body">
+                                            <h5 className="card-title">Card title</h5>
+                                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                                        </div>
+                                    </div>
+                                    <div className="card">
+                                        <div className="card-body">
+                                            <h5 className="card-title">Card title</h5>
+                                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                                        </div>
+                                    </div>
+                                </div>
                             </section>
                         </div>
                     </div>
