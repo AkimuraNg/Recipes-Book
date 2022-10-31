5f38dc0846c7435f3735c353cfc42f72af129e40
HNDuy Nguyen <duynguyen11062001@gmail.com>
2022-10-31 15:12:08 +0200

    Complete Cooking and Cocktail views layout

--- recipebook/src/components/cocktail/Cocktail.js
+++ recipebook/src/components/cocktail/Cocktail.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from '../navbar/navbar'
+import items from '../data/items.json'
 
 const Cocktail = () => {
   return (
@@ -13,6 +14,27 @@ const Cocktail = () => {
           <h1>Cocktail Recipes</h1>
           <p>This is a list of cocktail recipes</p>
         </section>
+
+        {/* Item list */}
+        <section className="list">
+          <div className="container-fluid">
+            <div className="row justify-content-center">
+              {items.map(post => {
+                return (
+                  <div className="card">
+                    <div className='card-body' key={post.id}>
+                      <img src={post.image} alt="recipe" />
+                      <h5 className="card-title">{post.title}</h5>
+                      <p className='card-text'>{post.desc}</p>
+                      <a href="#" className="btn btn-secondary">{post.links}</a>
+                    </div>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+        </section>
+
         <footer className="footer">
           <div className="container-fluid">
             <h3 style={{ color: 'white' }}>Copyrights 2022</h3>
