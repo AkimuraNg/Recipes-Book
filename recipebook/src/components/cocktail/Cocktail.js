305b605eb6626569efb0c54fc714e7ec0fd469a5
Tri Cao <tricq@flextech.vn>
2022-11-21 20:49:09 +0200

    add filter

--- recipebook/src/components/cocktail/Cocktail.js
+++ recipebook/src/components/cocktail/Cocktail.js
@@ -1,48 +1,74 @@
-import React from 'react'
-import Navbar from '../navbar/navbar'
-import items from '../data/cocktail.json'
+import React from "react";
+import Navbar from "../navbar/navbar";
+import items from "../data/cocktail.json";
+import { useState } from "react";
 
 const Cocktail = () => {
-  return (
-    <div>
-      <header>
-        <Navbar />
-      </header>
-      <main className='wrapper'>
-        {/* Intro */}
-        <section className='description'>
-          <h1>Cocktail Recipes</h1>
-          <p>This is a list of cocktail recipes</p>
-        </section>
+	const [item, setItem] = useState(items);
+	const [filter, setFilter] = useState("");
 
-        {/* Item list */}
-        <section className="list">
-          <div className="container-fluid">
-            <div className="row justify-content-center">
-              {items.map(post => {
-                return (
-                  <div className="card">
-                    <div className='card-body' key={post.id}>
-                      <img src={post.image} alt="recipe" />
-                      <h5 className="card-title">{post.title}</h5>
-                      <p className='card-text'>{post.desc}</p>
-                      <button className="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#recipeModal">View recipe</button>
-                    </div>
-                  </div>
-                )
-              })}
-            </div>
-          </div>
-        </section>
+	const itemToShow =
+		filter.length === 0
+			? item
+			: item.filter((p) =>
+					p.title.toLowerCase().includes(filter.toLowerCase())
+			  );
 
-        <footer className="footer">
-          <div className="container-fluid">
-            <h3 style={{ color: 'white' }}>Copyrights 2022</h3>
-          </div>
-        </footer>
-      </main>
-    </div>
-  )
-}
+	const handleChange = (e) => {
+		setFilter(e.target.value);
+	};
 
-export default Cocktail
+	return (
+		<div>
+			<header>
+				<Navbar />
+			</header>
+			<main className="wrapper">
+				{/* Intro */}
+				<section className="description">
+					<h1>Cocktail Recipes</h1>
+					<p>This is a list of cocktail recipes</p>
+				</section>
+
+				<input value={filter} onChange={handleChange} />
+
+				{/* Item list */}
+				<section className="list">
+					<div className="container-fluid">
+						<div className="row justify-content-center">
+							{itemToShow.map((post) => {
+								return (
+									<div className="card" key={post.id}>
+										<div className="card-body">
+											<h5 className="card-title">
+												{post.title}
+											</h5>
+											<p className="card-text">
+												{post.desc}
+											</p>
+											<button
+												className="btn btn-secondary"
+												data-bs-toggle="modal"
+												data-bs-target="#recipeModal"
+											>
+												View recipe
+											</button>
+										</div>
+									</div>
+								);
+							})}
+						</div>
+					</div>
+				</section>
+
+				<footer className="footer">
+					<div className="container-fluid">
+						<h3 style={{ color: "white" }}>Copyrights 2022</h3>
+					</div>
+				</footer>
+			</main>
+		</div>
+	);
+};
+
+export default Cocktail;
--- recipebook/src/components/cooking/Cooking.js
+++ recipebook/src/components/cooking/Cooking.js
@@ -1,49 +1,83 @@
-import React from 'react'
-import Navbar from '../navbar/navbar'
-import items from '../data/cooking.json'
-
+import React from "react";
+import Navbar from "../navbar/navbar";
+import items from "../data/cooking.json";
+import { useState } from "react";
 
 const Cooking = () => {
-  return (
-    <div className='Cooking-view'>
-      <header>
-        <Navbar />
-      </header>
-      <main className='wrapper'>
-        {/* Intro */}
-        <section className='description'>
-          <h1>Cooking Recipes</h1>
-          <p>This is a list of cooking recipes</p>
-        </section>
+	const [item, setItem] = useState(items);
+	const [filter, setFilter] = useState("");
 
-        {/* Item list */}
-        <section className="list">
-          <div className="container-fluid">
-            <div className="row justify-content-center">
-              {items.map(post => {
-                return (
-                  <div className="card">
-                    <div className='card-body' key={post.id}>
-                      <img src={post.image} alt="recipe" className="image" style={{cursor: 'pointer'}}/>
-                      <h5 className="card-title">{post.title}</h5>
-                      <p className='card-text'>{post.desc}</p>
-                      <button className="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#recipeModal">View recipe</button>
-                    </div>
-                  </div>
-                )
-              })}
-            </div>
-          </div>
-        </section>
+	const itemToShow =
+		filter.length === 0
+			? item
+			: item.filter((p) =>
+					p.title.toLowerCase().includes(filter.toLowerCase())
+			  );
 
-        <footer className="footer">
-          <div className="container-fluid">
-            <h3 style={{ color: 'white' }}>Copyrights 2022</h3>
-          </div>
-        </footer>
-      </main>
-    </div>
-  )
-}
+	const handleChange = (e) => {
+		setFilter(e.target.value);
+	};
 
-export default Cooking
+	return (
+		<div className="Cooking-view">
+			<header>
+				<Navbar />
+			</header>
+			<main className="wrapper">
+				{/* Intro */}
+				<section className="description">
+					<h1>Cooking Recipes</h1>
+					<p>This is a list of cooking recipes</p>
+				</section>
+
+				<input value={filter} onChange={handleChange} />
+
+				{/* Item list */}
+				<section className="list">
+					<div className="container-fluid">
+						<div className="row justify-content-center">
+							{itemToShow.map((post) => {
+								return (
+									<div className="card" key={post.id}>
+										<div
+											className="card-body"
+											key={post.id}
+										>
+											<img
+												src={post.image}
+												alt="recipe"
+												className="image"
+												style={{ cursor: "pointer" }}
+											/>
+											<h5 className="card-title">
+												{post.title}
+											</h5>
+											<p className="card-text">
+												{post.desc}
+											</p>
+											<button
+												className="btn btn-secondary"
+												data-bs-toggle="modal"
+												data-bs-target="#recipeModal"
+											>
+												View recipe
+											</button>
+										</div>
+									</div>
+								);
+							})}
+						</div>
+					</div>
+				</section>
+
+				<footer className="footer">
+					<div className="container-fluid">
+						<h3 style={{ color: "white" }}>Copyrights 2022</h3>
+					</div>
+				</footer>
+			</main>
+		</div>
+	);
+};
+
+export default Cooking;
