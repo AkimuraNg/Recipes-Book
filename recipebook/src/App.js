c4fd8140babf24ac7699860ac02a25d0068db6b9
HNDuy Nguyen <duynguyen11062001@gmail.com>
2022-11-21 15:30:58 +0200

    Change design, add functions, add page

--- recipebook/src/App.js
+++ recipebook/src/App.js
@@ -3,7 +3,8 @@ import Home from './components/Home';
 import { Routes, Route } from 'react-router-dom';
 import Cooking from './components/cooking/Cooking';
 import Cocktail from './components/cocktail/Cocktail';
-import Recipe from './components/recipe/recipe'
+import Modal from './components/Modal'
+import New from './components/New/new';
 
 function App() {
   return (
@@ -12,8 +13,10 @@ function App() {
         <Route path="" element={<Home />}></Route>
         <Route path="/cooking" element={<Cooking />}></Route>
         <Route path="/cocktail" element={<Cocktail />}></Route>
-        <Route path="/recipe" element = {<Recipe />}></Route>
+        <Route path="/new" element={<New />}></Route>
       </Routes>
+
+      <Modal/>
     </div>
   );
 }
